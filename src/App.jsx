46938d633d16ba126eeb5628c25feaab94fafdb0
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-07-18 20:25:26 -0300

    desenvolvido e implementado no site a pesquisa dos livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './componentes/header/index.jsx';
 import styled from 'styled-components';
+import Pesquisa from './componentes/pesquisa/index.jsx';
 
 const AppContainer = styled.div`  
     width: 100vw;
@@ -13,6 +14,7 @@ function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/componentes/pesquisa/dadosPesquisa.jsx
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png';
+
+export const livros = [
+  { nome: 'Liderança em Design', src: livro },
+  { nome: 'JavaScript Assertivo',  src: livro },
+  { nome: 'Cangaceiro em JavaScript', src: livro },
+  { nome: 'Aventureiros do Java', src: livro },
+  { nome: 'Liderança em UX', src: livro },
+];
