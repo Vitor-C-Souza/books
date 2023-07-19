664b254351370a80f7e9ab6db940cc9e0e2c9c07
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-07-19 20:51:19 -0300

    atualizado a estilização do projeto para corrigir um proplema de sobreposição de seções

--- src/componentes/pesquisa/index.jsx
+++ src/componentes/pesquisa/index.jsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { livros } from './dadosPesquisa.jsx';
 
-const PesquisaContainer = styled.section`
+const PesquisaContainer = styled.main`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
   color: #fff;
   text-align: center;
   padding: 85px 0;
-  height: 270px;
+  height: auto;
   width: 100%;
 `;
 
