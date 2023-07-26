501fa63f373b70c7cae858eb2845f807eaee2216
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-07-25 21:12:01 -0300

    criado os links do header para direcionar para outras paginas das rotas criadas

--- src/componentes/header/index.jsx
+++ src/componentes/header/index.jsx
@@ -3,19 +3,22 @@ import Logo from '../logo/index.jsx';
 import OpcoesHeader from '../opcoesHeader/index.jsx';
 import IconesHeader from '../iconesHeader/index.jsx';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
-    background-color: #FFF;
-    display: flex;
-    justify-content: space-evenly;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-evenly;
 `;
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <Logo />
+      <Link to={'/'}>
+        <Logo />
+      </Link>
       <OpcoesHeader />
-      <IconesHeader />        
+      <IconesHeader />
     </HeaderContainer>
   );
 }
